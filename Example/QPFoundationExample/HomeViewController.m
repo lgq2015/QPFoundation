f053b63f675ce1a6716f29d0e497a407509fd1be
Qiongpan Ke <keqiongpan@163.com>
2020-08-07 13:49:24 +0800

    [新增] 添加弹出新页面的示例代码。

--- Example/QPFoundationExample/HomeViewController.m
+++ Example/QPFoundationExample/HomeViewController.m
@@ -26,9 +26,62 @@
     return self;
 }
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
 
+    // 弹窗按钮
+
+    UIButton *popupButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [popupButton setTitle:@"Push a new ViewController" forState:UIControlStateNormal];
+    [popupButton setTitleColor:[UIColor whiteColor]
+                   forState:UIControlStateNormal];
+    [popupButton setBackgroundColor:[UIColor colorWithRGB:0x339900]
+                        forState:UIControlStateNormal];
+    [popupButton setClipsToBounds:YES];
+    [popupButton.layer setCornerRadius:8.0];
+    [self.view addSubview:popupButton];
+
+    [popupButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.size.mas_equalTo(CGSizeMake(256.0, 48.0));
+        make.center.equalTo(popupButton.superview).with.centerOffset(CGPointMake(0.0, -48.0));
+    }];
+
+    [popupButton addTarget:self
+                 action:@selector(onPopupButtonClick)
+       forControlEvents:UIControlEventTouchUpInside];
+
+    // 调用网络接口按钮
+
+    UIButton *invokeButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [invokeButton setTitle:@"Invoke network API" forState:UIControlStateNormal];
+    [invokeButton setTitleColor:[UIColor whiteColor]
+                       forState:UIControlStateNormal];
+    [invokeButton setBackgroundColor:[UIColor colorWithRGB:0x339900]
+                            forState:UIControlStateNormal];
+    [invokeButton setClipsToBounds:YES];
+    [invokeButton.layer setCornerRadius:8.0];
+    [self.view addSubview:invokeButton];
+
+    [invokeButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.size.mas_equalTo(CGSizeMake(256.0, 48.0));
+        make.center.equalTo(invokeButton.superview).with.centerOffset(CGPointMake(0.0, 48.0));
+    }];
+
+    [invokeButton addTarget:self
+                     action:@selector(onInvokeButtonClick)
+           forControlEvents:UIControlEventTouchUpInside];
+}
+
+- (void)onPopupButtonClick
+{
+    QPPanelController *blank = [[QPPanelController alloc] init];
+    blank.title = @"Blank";
+    [QPGetPageNavigationController() pushViewController:blank animated:YES];
+}
+
+- (void)onInvokeButtonClick
+{
     // insert code here...
 }
 
