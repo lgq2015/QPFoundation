8ba7dea8a82ada8f0f2d30235ec09ea6d3f63a2a
Qiongpan Ke <keqiongpan@163.com>
2020-08-08 10:14:27 +0800

    [新增] 添加QPScrollPanelController相关的使用示例代码。

--- Example/QPFoundationExample/DetailViewController.m
+++ Example/QPFoundationExample/DetailViewController.m
@@ -30,6 +30,32 @@
 {
     [super viewDidLoad];
 
+    // 设置内容视图大小，使之可以被滚动起来。
+
+    [self.contentView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.height.mas_equalTo(2000.0);
+    }];
+
+    // 顶部提示信息
+
+    UILabel *topTipLabel = [[UILabel alloc] init];
+    [topTipLabel setText:@"Scroll to bottom ..."];
+    [topTipLabel setFont:[UIFont boldSystemFontOfSize:24.0]];
+    [self.contentView addSubview:topTipLabel];
+    [topTipLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.and.centerX.equalTo(self.contentView);
+    }];
+
+    // 底部提示信息
+
+    UILabel *bottomTipLabel = [[UILabel alloc] init];
+    [bottomTipLabel setText:@"BOTTOM!"];
+    [bottomTipLabel setFont:[UIFont boldSystemFontOfSize:32.0]];
+    [self.contentView addSubview:bottomTipLabel];
+    [bottomTipLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.bottom.and.centerX.equalTo(self.contentView);
+    }];
+
     // 顶部栏
 
     QPCustomBar *topCustomBar = [[QPCustomBar alloc] init];
@@ -43,6 +69,21 @@
     [topCustomBar addSubview:topLabel];
     [topLabel mas_makeConstraints:^(MASConstraintMaker *make) {
         make.edges.equalTo(topCustomBar);
+    }];
+
+    // 附加栏
+
+    QPCustomBar *additionCustomBar = [[QPCustomBar alloc] init];
+    [additionCustomBar setBackgroundColor:[UIColor colorWithRGB:0xffcccc]];
+    [additionCustomBar mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.height.mas_equalTo(128.0);
+    }];
+    [self addBottomCustomBar:additionCustomBar];
+
+    UISwitch *additionSwitch = [[UISwitch alloc] init];
+    [additionCustomBar addSubview:additionSwitch];
+    [additionSwitch mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.center.equalTo(additionCustomBar);
     }];
 
     // 底部栏
